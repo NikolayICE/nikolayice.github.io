0022c665d26281cafa97afdc7f8229af9971b464
NikolayICE <kryachko2901@gmail.com>
2025-02-27 20:16:54 +0300

    edits 9

--- js/main.js
+++ js/main.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const reveals = document.querySelectorAll('.reveal');
-  
-    const options = {
-      threshold: 0.2
-    };
-  
+    const options = { threshold: 0.2 };
     const observer = new IntersectionObserver((entries) => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('active');
+            entry.target.classList.add('active');
         } else {
-          entry.target.classList.remove('active');
+            entry.target.classList.remove('active');
         }
       });
     }, options);
-  
     reveals.forEach(reveal => {
       observer.observe(reveal);
     });
