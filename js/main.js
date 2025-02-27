0f9d28aee18497457a6c025765088edb65bee190
NikolayICE <kryachko2901@gmail.com>
2025-02-27 21:30:13 +0300

    edits 10

--- js/main.js
+++ js/main.js
@@ -2,16 +2,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const reveals = document.querySelectorAll('.reveal');
     const options = { threshold: 0.2 };
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
+        entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('active');
-        } else {
-            entry.target.classList.remove('active');
         }
       });
     }, options);
     reveals.forEach(reveal => {
-      observer.observe(reveal);
+        observer.observe(reveal);
     });
   });
   
